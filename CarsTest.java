7f4080251e3ee80240d01ea2490414fd17a0f72a
Danne <daniel.kindvall05@gmail.com>
2025-01-23 10:38:30 +0100

    Work on tests

--- CarsTest.java
+++ CarsTest.java
@@ -59,17 +59,92 @@ public class CarsTest {
         Assert.assertEquals(62.6, volvo.getPosition()[1], 0.0001);
 
 
+        double previousSpeed = volvo.getCurrentSpeed();
+        volvo.gas(-4);
+        Assert.assertEquals(previousSpeed, volvo.getCurrentSpeed(), 0);
+        volvo.gas(1000);
+        Assert.assertEquals(previousSpeed + 0.01*1.25*100, volvo.getCurrentSpeed(), 0);
+        volvo.incrementSpeed(1000);
+        Assert.assertTrue(volvo.getCurrentSpeed() > previousSpeed);
+        Assert.assertEquals(volvo.getCurrentSpeed(), volvo.getEnginePower(), 0);
     }
 
     // Saab tests
     @Test
     public void testSaabInstantiation() {
         Assert.assertEquals(2, saab.getNrDoors());
+        Assert.assertEquals(125.0, saab.getEnginePower(), 0);
+        Assert.assertEquals(Color.red, saab.getColor());
+        Assert.assertEquals("Saab95", saab.getModelName());
+        Assert.assertEquals(0.0, saab.getCurrentSpeed(), 0);
     }
 
     @Test
     public void testSaabMovement() {
         saab.startEngine();
         Assert.assertEquals(0.1, saab.getCurrentSpeed(), 0);
+
+        saab.startEngine();
+        Assert.assertEquals(0.1, saab.getCurrentSpeed(), 0.0001);
+
+        saab.incrementSpeed(50);
+        Assert.assertEquals(62.6, saab.getCurrentSpeed(), 0);
+
+
+
+        saab.move();
+        Assert.assertEquals(62.6, saab.getPosition()[0], 0.0001);
+        Assert.assertEquals(0, saab.getPosition()[1], 0.0001);
+        saab.move();
+        Assert.assertEquals(125.2, saab.getPosition()[0], 0.0001);
+        Assert.assertEquals(0, saab.getPosition()[1], 0.0001);
+
+
+        Assert.assertEquals(1, saab.getDirection()[0], 0.0001);
+        Assert.assertEquals(0, saab.getDirection()[1], 0.0001);
+        saab.turnLeft();
+        Assert.assertEquals(0, saab.getDirection()[0], 0.0001);
+        Assert.assertEquals(1, saab.getDirection()[1], 0.0001);
+        saab.move();
+        Assert.assertEquals(125.2, saab.getPosition()[0], 0.0001);
+        Assert.assertEquals(62.6, saab.getPosition()[1], 0.0001);
+
+        saab.turnLeft();
+        saab.turnLeft();
+        saab.turnLeft();
+        saab.move();
+        Assert.assertEquals(187.8, saab.getPosition()[0], 0.0001);
+        Assert.assertEquals(62.6, saab.getPosition()[1], 0.0001);
+
+        saab.turnRight();
+        saab.turnRight();
+        saab.move();
+        saab.move();
+        saab.move();
+        saab.move();
+        Assert.assertEquals(-62.6, saab.getPosition()[0], 0.0001);
+        Assert.assertEquals(62.6, saab.getPosition()[1], 0.0001);
+
+
+        saab.stopEngine();
+        Assert.assertEquals(0.0, saab.getCurrentSpeed(), 0);
+
+        saab.startEngine();
+        Assert.assertEquals(0.1, saab.getCurrentSpeed(), 0);
+
+        saab.setTurboOn();
+        Assert.assertTrue(saab.getTurboStatus());
+
+        saab.incrementSpeed(50);
+        Assert.assertEquals(0.1 + (125*1.3*0.01*50), saab.getCurrentSpeed(), 0);
+    }
+
+    @Test
+    public void testGasBreak(){
+
+
+
+
+
     }
 }
--- Saab95.java
+++ Saab95.java
@@ -1,9 +1,6 @@
 import java.awt.*;
 
 public class Saab95 extends TurboCar{
-
-    public boolean turboOn;
-    
     public Saab95(){
         super(2, 125, Color.red, "Saab95");
     }
